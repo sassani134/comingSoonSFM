767af9ad4d80857f96bcd69c1724a9ea30ee311b
Francis Scaringella <francis.scaringella@brassart.fr>
2022-04-12 17:08:25 +0200

    réalisation du mode dessin sur le canvas

--- js/change-color.js
+++ js/change-color.js
@@ -1,4 +1,42 @@
-let body = document.getElementsByTagName('body');
+function handleSubmit() {
+	let hexNumbers = [
+		"0",
+		"1",
+		"2",
+		"3",
+		"4",
+		"5",
+		"6",
+		"7",
+		"8",
+		"9",
+		"A",
+		"B",
+		"C",
+		"D",
+		"E",
+		"F",
+	];
+
+  	let hexColorCode = "";
+	for (var i = 0; i < 6; i++) {
+		let randomIndex = Math.floor(Math.random() * hexNumbers.length);
+		hexColorCode += hexNumbers[randomIndex];
+	}
+
+	// document.getElementById("colorCode").innerHTML = hexColorCode;
+	document.getElementsByTagName("body")[0].style.background = "#" + hexColorCode;
+}
+
+document.getElementById("section1").addEventListener("click", function() {
+	handleSubmit();
+});
 
 
-console.log(dotColor);
+
+// let body = document.getElementsByTagName('body');
+
+// body.addEventListener("click", function() {
+// 	body.style.backgroundColor
+// });
+
